92f126eb346baef836b34192a234679d31b80df3
unknown <daniel.torres.84@gmail.com>
2019-03-19 12:43:59 -0700

    found the filepath and extension, just need to loop through array next and spit out files

--- filteredList.js
+++ filteredList.js
@@ -1,13 +1,24 @@
 var fs = require('fs');
-var path = process.argv[2];
+var path = require('path');
+var filePath = process.argv[2];
 
-fs.readdir(path, function callback(err, list){
+
+console.log(`the filepath is: ${filePath} and the extension is: ${process.argv[3]}`);
+/*
+fs.readdir(filePath, function callback(err, list){
 	if (err) {
 		throw err;
 	} else {
-
+		path.extname(list);
 	}
 });
+
+*/
+/*
+-----------Notes----------------
+list = [list of filname strings]
+I want to loop through that list 
+*/
 
 /*
 resources: file:///C:/Users/Daniel/AppData/Roaming/npm/node_modules/learnyounode/node_apidoc/path.html
