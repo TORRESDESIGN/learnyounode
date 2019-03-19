2f352495229ed894e28e7765a57fa38b4cb78f33
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2019-03-18 20:55:30 -0700

    worked on filteredList.js

--- filteredList.js
+++ filteredList.js
@@ -1,1 +1,14 @@
 var fs = require('fs');
+var path = process.argv[2];
+
+fs.readdir(path, function callback(err, list){
+	if (err) {
+		throw err;
+	} else {
+
+	}
+});
+
+/*
+resources: file:///C:/Users/Daniel/AppData/Roaming/npm/node_modules/learnyounode/node_apidoc/path.html
+*/
