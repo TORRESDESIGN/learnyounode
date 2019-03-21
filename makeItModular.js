b20e24cdef97db5c137475e37df6593ad2738f0a
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2019-03-20 22:13:53 -0700

    Worked on my first Modular file.

--- makeItModular.js
+++ makeItModular.js
@@ -1,3 +1,18 @@
+var fs = require('fs');
+
+module.exports.myModule = function(dir, ext, callback) {
+	
+}
+
 /*
 This requires 2 files to complete
+
+----------Plan----------------
+1. print a filtered list of files from a given dir, by given ext
+2. The 1st arg is dir, and 2nd arg is the ext
+3. Print the list of files, one per line in the console(async io)
+
+4. Create a module file to do most of work.
+5. File must export 1 function that takes 3 args(dir, ext, callback func
+6. 
 */
