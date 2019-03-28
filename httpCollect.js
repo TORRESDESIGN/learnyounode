a0d9e40ef22ee3cd42a16f5d9efb7aa7ffc01205
unknown <daniel.torres.84@gmail.com>
2019-03-28 12:28:38 -0700

    Worked on httpCollect, I need to reread the directions though

--- httpCollect.js
+++ httpCollect.js
@@ -2,5 +2,24 @@ const http = require('http');
 const url = process.argv[2];
 
 http.get(url, (resp) => {
-	
+	resp.setEncoding('utf8');
+	let allData;
+
+	resp.on("data", function(data) {
+		allData = allData + data;
+	});
+
+	resp.on("end", function() {
+		console.log(allData);
+	});
+}).on("error", function(err) {
+	console.log(err);
 });
+
+/*
+-----------Plan------------
+
+
+
+
+*/
