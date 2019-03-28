127ff0f76c9c8df3125bc0a2620c96e8a0b27870
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2019-03-27 21:36:53 -0700

    finished httpClient.js and started the next

--- httpClient.js
+++ httpClient.js
@@ -1,7 +1,8 @@
 var http = require('http');
-var url = require(process.argv[2]);
+var url = process.argv[2];
 
-http.get(url, utf8, function (response) {
+http.get(url, function (response) {
+	response.setEncoding("utf8");
 	//stream object(emit events)
 	//[data, error, end]
 	response.on("data", function (data) {
@@ -9,7 +10,13 @@ http.get(url, utf8, function (response) {
 	});
 })
 /*
---------Plan----------
+--------Alternative to mine----------
+var http = require('http')
 
+http.get(process.argv[2], function (response) {
+	response.setEncoding('utf8')
+	response.on('data', console.log)
+	response.on('error', console.error)
+}).on('error', console.error)
 
 */
