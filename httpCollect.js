1c0023e27efa0afee58e077a02707e2a1d5a9e40
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2019-04-01 20:10:12 -0700

    finished http Collect excercise

--- httpCollect.js
+++ httpCollect.js
@@ -3,13 +3,17 @@ const url = process.argv[2];
 
 http.get(url, (resp) => {
 	resp.setEncoding('utf8');
-	let allData;
-
+	let allData = [];
+	let n;
 	resp.on("data", function(data) {
-		allData = allData + data;
+		if (data) {
+			allData.push(data);
+		}
 	});
-
 	resp.on("end", function() {
+		n = allData.join("").length;
+		allData = allData.join("");
+		console.log(n);
 		console.log(allData);
 	});
 }).on("error", function(err) {
@@ -17,8 +21,20 @@ http.get(url, (resp) => {
 });
 
 /*
------------Plan------------
+-----------Alternate Solution using 3rd party package(bl: Buffer List)------------
+var http = require('http')
+var bl = require('bl')
 
+http.get(process.argv[2], function (response) {
+  response.pipe(bl(function (err, data) {
+    if (err) {
+      return console.error(err)
+    }
+    data = data.toString()
+    console.log(data.length)
+    console.log(data)
+  }))
+})
 
 
 
