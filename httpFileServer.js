a78f61dbbfdb9c4a1a8d694c029d92f5c645f6cb
unknown <daniel.torres.84@gmail.com>
2019-04-08 12:49:30 -0700

    finished httpFileServer excercise

--- httpFileServer.js
+++ httpFileServer.js
@@ -1,8 +1,25 @@
 const http = require('http');
+const fs = require('fs');
 const port = process.argv[2];
-const location = process.argv[3];
+const fileLocation = process.argv[3];
 
 var server = http.createServer((req, res) => {
+	fs.createReadStream(fileLocation).pipe(res);
 
 });
 server.listen(port);
+
+/*-----------Alteranate Solution
+
+var http = require('http')
+var fs = require('fs')
+
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { 'content-type': 'text/plain' })
+
+  fs.createReadStream(process.argv[3]).pipe(res)
+})
+
+server.listen(Number(process.argv[2]))
+
+*/
