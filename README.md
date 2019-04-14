819e7cd6fcb9c5bebf06753f5bf77593eecba6df
unknown <daniel.torres.84@gmail.com>
2019-04-14 12:50:26 -0700

    worked on last excercise

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # learnyounode
 Node practice from https://nodeschool.io/
+
+Excercise List
+1. Hello World
+2. Baby Steps
+3. My First I/O
+4. My First ASYNC I/O
+5. Filtered LS
+6. Make it Modular
+7. HTTP Client
+8. HTTP Collect
+9. Juggling ASYNC
+10. Time Server
+11. HTTP File Server
+12. HTTP Upper Caserer
+13. HTTP JSON API Server <--- Currently on
--- httpJsonApiServ.js
+++ httpJsonApiServ.js
@@ -1,4 +1,4 @@
-const http = require('require');
+const http = require('http');
 const url = require('url');
 const port = process.argv[2];
 
@@ -6,6 +6,13 @@ const path = '/api/parsetime';
 
 
 var server = http.createServer((req, res) => {
+	res.writeHead(200, { 'content-type': 'application/json' });
 	fs.createReadStream(path).pipe(res);
 });
 server.listen(port);
+
+
+/*
+Offline Docs: file:///C:/Users/Floater/AppData/Roaming/npm/node_modules/learnyounode/node_apidoc/
+
+*/
