5ba8eb6d7cdeab0c92ba87fe67824ae40bf299cd
unknown <daniel.torres.84@gmail.com>
2019-04-15 12:44:36 -0700

    makes a bit more sense now, need to finish still

--- httpJsonApiServ.js
+++ httpJsonApiServ.js
@@ -1,13 +1,15 @@
 const http = require('http');
+//const fs = require('fs');// node -pe "require('url').parse('/test?q=1', true)"
 const url = require('url');
 const port = process.argv[2];
 
-const path = '/api/parsetime';
+//const path = '/api/parsetime';
 
 
 var server = http.createServer((req, res) => {
 	res.writeHead(200, { 'content-type': 'application/json' });
-	fs.createReadStream(path).pipe(res);
+	var myUrl = url.parse(req.url, true);
+	console.log(myUrl);// shows url data now
 });
 server.listen(port);
 
