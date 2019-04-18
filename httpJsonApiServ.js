f440eb76c79587bb5be2930e1a20daac33d73fac
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2019-04-17 21:34:16 -0700

    still working on it

--- httpJsonApiServ.js
+++ httpJsonApiServ.js
@@ -7,14 +7,17 @@ const port = process.argv[2];
 //const path = '/api/parsetime';
 
 var server = http.createServer((req, res) => {
-	res.writeHead(200, { 'content-type': 'application/json' });
-	var myUrl = url.parse(req.url, true);
-	console.log(myUrl);// shows url data now
+	if (req.method == 'GET') {
+		res.writeHead(200, { 'content-type': 'application/json' });
+		var myUrl = url.parse(req.url, true);
+		console.log(myUrl);
+		res.end('ok');// shows url data now
+	}
 });
 server.listen(port);
 
 
 /*
-Offline Docs: file:///C:/Users/Floater/AppData/Roaming/npm/node_modules/learnyounode/node_apidoc/
+Offline Docs: file:///C:/Users/Daniel/AppData/Roaming/npm/node_modules/learnyounode/node_apidoc/
 
 */
